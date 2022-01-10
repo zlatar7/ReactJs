fe5a509f138f54f3c98259465e3e7206d1ac8bd3
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-01-09 21:04:07 -0300

    Clase 3

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+export default function NavBar (){
+
+    return(
+    <>
+        <div>
+            <ul className="App-header">
+                <li>INICIO</li>
+                <li>PRODUCTOS</li>
+                <li>PREGUNTAS FRECUENTES</li>
+                <li>CONTACTO</li>
+            </ul>
+        </div>
+    </>
+    )
+    
+}
